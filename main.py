3950752c01f30304685aec332de660298161d29a
jarico <3111825083@qq.com>
2025-12-12 14:06:12 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+import tkinter as tk
+from tkinter import ttk
+from main_window import MainWindow
+
+def main():
+    root = tk.Tk()
+    app = MainWindow(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
